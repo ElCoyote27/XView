004659441058febe0f507975325c65c09a9c5743
Vincent S. Cojot <vincent@cojot.name>
2010-02-17 11:47:10 +0000

    *** empty log message ***

--- scripts/build_zip.sh
+++ scripts/build_zip.sh
@@ -4,7 +4,7 @@ TMP_DIR=$(cd `dirname $0`;cd ..;pwd)
 SRC_DIR=$(basename $TMP_DIR)
 BASE_DIR=$(dirname $TMP_DIR)
 ZIP_NAME="${HOME}/ow${CUR_DATE}.zip"
-echo "Building ${ZIP_NAME}.."
+echo "Building ${ZIP_NAME} from ${BASE_DIR}/${SRC_DIR}.."
 cd ${BASE_DIR}
 #zip -9qr ${ZIP_NAME} ${SRC_DIR} -x */i386-Linux/*
 zip -9qr ${ZIP_NAME} ${SRC_DIR}
