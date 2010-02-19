7746889b1b1599f4fb1db7144fe40b61d96efcaa
Vincent S. Cojot <vincent@cojot.name>
2010-02-19 17:02:32 +0000

    *** empty log message ***

--- scripts/build_zip.sh
+++ scripts/build_zip.sh
@@ -3,7 +3,15 @@ CUR_DATE=`date '+%Y%m%d'`
 TMP_DIR=$(cd `dirname $0`;cd ..;pwd)
 SRC_DIR=$(basename $TMP_DIR)
 BASE_DIR=$(dirname $TMP_DIR)
-ZIP_NAME="${HOME}/ow${CUR_DATE}.zip"
+
+# Get version from library
+if [ -f ${BASE_DIR}/${SRC_DIR}/libdeskset/ds_relname.h ]; then
+	CUR_VERSION=`awk '{if ($2 ~ /_RELEASE/ ) print $3}' ${BASE_DIR}/${SRC_DIR}/libdeskset/ds_relname.h|sed -e 's/"//g' -e 's/v//'`
+else
+	CUR_VERSION="ALPHA"
+fi
+
+ZIP_NAME="${HOME}/OpenWindows-${CUR_VERSION}-${CUR_DATE}.zip"
 echo "Building ${ZIP_NAME} from ${BASE_DIR}/${SRC_DIR}.."
 cd ${BASE_DIR}
 #zip -9qr ${ZIP_NAME} ${SRC_DIR} -x */i386-Linux/*
