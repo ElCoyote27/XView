e04a6ea6676a0fec3e64db3b5e9aa27144b1a8b1
Vincent S. Cojot <vincent@cojot.name>
2010-03-01 12:23:49 +0000

    *** empty log message ***

--- scripts/build_zip.sh
+++ scripts/build_zip.sh
@@ -14,6 +14,6 @@ fi
 ZIP_NAME="${HOME}/OpenWindows-${CUR_VERSION}-${CUR_DATE}.zip"
 echo "Building ${ZIP_NAME} from ${BASE_DIR}/${SRC_DIR}.."
 cd ${BASE_DIR}
-# zip -9qr ${ZIP_NAME} ${SRC_DIR} -x */i386-Linux/* -e */sparc-SunOS/*
-zip -9qr ${ZIP_NAME} ${SRC_DIR} -x *.o
+# zip -9qr ${ZIP_NAME} ${SRC_DIR} -x */i386-Linux/* -x */sparc-SunOS/*
+zip -9qr ${ZIP_NAME} ${SRC_DIR} -x *.o -x core
 
