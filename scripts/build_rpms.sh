0e52df7190cc48164ad3850e1d81de22a9cb5c6c
Vincent S. Cojot <vincent@cojot.name>
2010-03-09 10:03:10 +0000

    Initial revision

--- scripts/build_rpms.sh
+++ scripts/build_rpms.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-CUR_DATE=$(date '+%Y%m%d')
+CUR_DATE=`date '+%Y%m%d'`
 TMP_DIR=$(cd `dirname $0`;pwd)
 SRC_DIR=$(basename $TMP_DIR)
 BASE_DIR=$(dirname $TMP_DIR)
-SPEC_FILE=${BASE_DIR}/${SRC_DIR}/XView.spec
-RPM_BASE_DIR=${BASE_DIR}/build
-RPM_BUILD_DIR=${RPM_BASE_DIR}/BUILD/$(uname -n)
-RPM_TMP_PATH=${RPM_BASE_DIR}/tmp/$(uname -n)
-
-VERSION="$(awk '{ print $7 }' /etc/redhat-release|cut -c1)"
-DIST=".el${VERSION}"
-rpmextras=""
+SPEC_FILE=${BASE_DIR}/${SRC_DIR}/OpenWindows.spec
+RPM_BASE_DIR=${BASE_DIR}/rpms
 
 # Find spec file...
 if [ ! -f ${SPEC_FILE} ]; then
@@ -18,45 +12,17 @@ if [ ! -f ${SPEC_FILE} ]; then
 fi
 
 # Make sure the base dirs are all there...
-for mydir in ${RPM_BASE_DIR}/SOURCES ${RPM_BASE_DIR}/SRPMS ${RPM_BASE_DIR}/RPMS ${RPM_BASE_DIR}/SPECS ${RPM_BASE_DIR}/BUILD ${RPM_BASE_DIR} ${RPM_BASE_DIR}/BUILD/$(uname -i) ${RPM_BUILD_DIR} ${RPM_TMP_PATH}
+for mydir in ${RPM_BASE_DIR}/SRPMS ${RPM_BASE_DIR}/RPMS ${RPM_BASE_DIR}/SPECS ${RPM_BASE_DIR}/BUILD ${RPM_BASE_DIR}/tmp
 do
 	if [ ! -d ${mydir} ]; then
-		mkdir -vp ${mydir} || exit 1
-	fi
+		mkdir -p ${mydir} || exit 1
+	fi	
 done
 
-#
-#grep -q "^%define BaseRelease ${BASE_RELEASE}\$" ${SPEC_FILE}
-#if [ $? -eq 1 ]; then
-#	echo "BaseRelease mismatch! Editing ${SPEC_FILE} with \"BaseRelease: ${BASE_RELEASE}\"..."
-#	perl -pi -e "s@^%define BaseRelease .*\$@%define BaseRelease ${BASE_RELEASE}@" ${SPEC_FILE}
-#fi
-
-# Checks for libhal
-#if [ -x /usr/bin/pkg-config ]; then
-#	/usr/bin/pkg-config  --silence-errors hal
-#	libhal=$?
-#	/usr/bin/pkg-config  --silence-errors dbus-glib-1
-#	dbus=$?
-#	if [ $dbus -eq 0 -a $libhal -eq 0 ]; then
-#		echo 'Using hal and dbus/dbus-glibc-1 for xvfilemgr..'
-#		rpmextras="--with libhal"
-#	else
-#		echo 'Unable to use hal and/or dbus-glibc-1 for xvfilemgr, Skipping...'
-#	fi
-#fi
-
-if [ -x /usr/bin/linux32 ]; then
-	BOOTSTRAP=/usr/bin/linux32
-else
-	BOOTSTRAP=
-fi
-
-${BOOTSTRAP} /usr/bin/rpmbuild -ba \
-	${rpmextras} --sign \
-	--target i386 \
-	--define "dist ${DIST}" \
-	--define "_topdir ${RPM_BASE_DIR}" \
-	--define "_builddir ${RPM_BUILD_DIR}" \
-	--define "_tmppath ${RPM_TMP_PATH}" \
+# perform build with hard-coded dist define...
+/usr/bin/rpmbuild -ba \
+	--define "dist .el5" \
+	--define "%_topdir ${RPM_BASE_DIR}" \
+	--define "%_tmppath ${RPM_BASE_DIR}/tmp" \
 	${SPEC_FILE}
+
