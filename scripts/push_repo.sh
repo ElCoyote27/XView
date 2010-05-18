a663c63b77aab55a2b20e2eabd392b1288d09c87
Vincent S. Cojot <vincent@cojot.name>
2010-05-18 08:34:14 +0000

    *** empty log message ***

--- scripts/push_repo.sh
+++ scripts/push_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Rev: $Id: push_repo.sh,v 1.1 2010/05/18 08:33:33 vcojot Exp $
+# Rev: $Id: push_repo.sh,v 1.2 2010/05/18 08:34:14 vcojot Exp $
 
 # Local variables
 CUR_DATE=`date '+%Y%m%d'`
@@ -10,4 +10,4 @@ RPM_BASE_DIR=${BASE_DIR}/build
 DEST_DIR=thor:Vincent/Code
 
 # Main routine
-rsync -avzn --exclude=${RPM_BASE_DIR}/BUILD/\* ${BASE_DIR} ${DEST_DIR}
+rsync -avz --exclude=${RPM_BASE_DIR}/BUILD/\* ${BASE_DIR} ${DEST_DIR}
