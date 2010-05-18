9da2648320c5d4e69fc425f1c9c85c397f046974
Vincent S. Cojot <vincent@cojot.name>
2010-05-18 08:33:33 +0000

    Initial revision

--- scripts/push_repo.sh
+++ scripts/push_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Rev: $Id: push_repo.sh,v 1.7 2013/01/02 14:32:29 raistlin Exp $
+# Rev: $Id: push_repo.sh,v 1.1 2010/05/18 08:33:33 vcojot Exp $
 
 # Local variables
 CUR_DATE=`date '+%Y%m%d'`
@@ -7,13 +7,7 @@ TMP_DIR=$(cd `dirname $0`;pwd)
 SRC_DIR=$(basename $TMP_DIR)
 BASE_DIR=$(dirname $TMP_DIR)
 RPM_BASE_DIR=${BASE_DIR}/build
-DEST_DIR=thor:World/Vincent/Code
-
-#
-RSYNC_CMD=rsync
-RSYNC_OPTIONS="-avz \
-	--exclude=`basename ${BASE_DIR}`/build/BUILD \
-	--exclude=`basename ${BASE_DIR}`/build/tmp"
+DEST_DIR=thor:Vincent/Code
 
 # Main routine
-${RSYNC_CMD} ${RSYNC_OPTIONS} ${BASE_DIR} ${DEST_DIR}
+rsync -avzn --exclude=${RPM_BASE_DIR}/BUILD/\* ${BASE_DIR} ${DEST_DIR}
