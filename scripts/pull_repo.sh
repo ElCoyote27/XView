1a6d6f1a9a4610de9a0f985bdf65be29196ee125
Vincent S. Cojot <vincent@cojot.name>
2012-05-21 10:30:39 +0000

    Initial revision

--- scripts/pull_repo.sh
+++ scripts/pull_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Rev: $Id: pull_repo.sh,v 1.4 2013/01/03 10:41:51 raistlin Exp $
+# Rev: $Id: pull_repo.sh,v 1.1 2012/05/21 10:30:39 vcojot Exp $
 
 # Local variables
 CUR_DATE=`date '+%Y%m%d'`
@@ -7,11 +7,11 @@ TMP_DIR=$(cd `dirname $0`;pwd)
 DEST_DIR=$(basename $TMP_DIR)
 BASE_DIR=$(dirname $TMP_DIR)
 RPM_BASE_DIR=${BASE_DIR}/build
-SRC_DIR=thor:World/Vincent/Code/XView
+SRC_DIR=thor:Vincent/Code/XView
 
 #
 RSYNC_CMD=rsync
-RSYNC_OPTIONS="-auvz --bwlimit=2048 --stats --delete \
+RSYNC_OPTIONS="-auvzn --bwlimit=2048 --stats \
 	--exclude=`basename ${BASE_DIR}`/build/BUILD \
 	--exclude=`basename ${BASE_DIR}`/build/tmp"
 
