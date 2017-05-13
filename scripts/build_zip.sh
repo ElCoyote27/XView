7f9f846b2c6571fc9c9e102702da33615ee104a4
Vincent S. Cojot <vincent@cojot.name>
2017-05-13 17:44:04 -0400

    Initial Revision..

--- scripts/build_zip.sh
+++ scripts/build_zip.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 CUR_DATE=`date '+%Y%m%d'`
-TMP_DIR=$(cd `dirname $0`;cd ..;pwd)
-SRC_DIR=$(basename $TMP_DIR)
-BASE_DIR=$(dirname $TMP_DIR)
+#
+[ "$BASH" ] && function whence
+{
+	type -p "$@"
+}
+#
+BASE_DIR="$(cd $(/usr/bin/dirname $(whence -- $0 || echo $0));cd ..;pwd)"
+SRC_DIR="XView"
 
 # Get version from library
-if [ -f ${BASE_DIR}/${SRC_DIR}/libdeskset/ds_relname.h ]; then
-	CUR_VERSION=`awk '{if ($2 ~ /_RELEASE/ ) print $3}' ${BASE_DIR}/${SRC_DIR}/libdeskset/ds_relname.h|sed -e 's/"//g' -e 's/v//'`
+if [ -f ${BASE_DIR}/${SRC_DIR}/lib/libxview/base/xview_impl.h ]; then
+	CUR_VERSION="3.2p1.4"
 else
 	CUR_VERSION="ALPHA"
 fi
 
-ZIP_NAME="${BASE_DIR}/${SRC_DIR}/build/SOURCES/OWacomp-${CUR_VERSION}-${CUR_DATE}.zip"
+ZIP_NAME="${BASE_DIR}/build/SOURCES/XView-${CUR_VERSION}-${CUR_DATE}.zip"
 
-echo "Building ${ZIP_NAME} from ${BASE_DIR}/${SRC_DIR}.."
-OUT_DIR=`dirname ${ZIP_NAME}`
+echo "Building ${ZIP_NAME} from ${BASE_DIR}/${SRC_DIR} ..."
+OUT_DIR=$(dirname ${ZIP_NAME})
 if [ ! -d ${OUT_DIR} ]; then
 	mkdir -p ${OUT_DIR}
 fi
 
-if [ -x /usr/bin/rcsclean ]; then
-	/usr/bin/rcsclean -n ${BASE_DIR}/${SRC_DIR}/{ce,devguide/libguide,devguide/libguidexv,filemgr,libdeskset,mailtool,ftptool,clients/*/*}/*.{c,h,sh} \
-	${BASE_DIR}/${SRC_DIR}/{ce,devguide/libguide,devguide/libguidexv,filemgr,libdeskset,mailtool,ftptool,clients/*}/Makefile \
-	${BASE_DIR}/${SRC_DIR}/Makefile.include
-fi
-
 if [ -x /usr/bin/zip ]; then
 	cd ${BASE_DIR}
-	# /usr/bin/zip -9qr ${ZIP_NAME} ${SRC_DIR} -x */i386-Linux/* -x */sparc-SunOS/*
 	/usr/bin/zip -9qr ${ZIP_NAME} ${SRC_DIR} -x '*.o' -x 'core' -x "${SRC_DIR}/build/*"
 fi
-
